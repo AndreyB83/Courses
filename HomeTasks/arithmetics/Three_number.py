3240fbe84f677e874863da3fe440c9690d821172
Andrey Bobrovich <andreyidei@bk.ru>
2022-09-01 00:10:56 +0300

    add exceptions

--- HomeTasks/arithmetics/Three_number.py
+++ HomeTasks/arithmetics/Three_number.py
@@ -1,7 +1,14 @@
-number_abc = int(input('Введите трехзначное целое число:'))
-number_ab = number_abc // 10
-number_c = number_abc % 10
-number_a = number_ab // 10
-number_b = number_ab % 10
-sum_abc = number_a + number_b + number_c
-print('Сумма чисел равна:', sum_abc)
+# Пользователь вводит  трехзначное число.
+# Необходимо найти сумму цифр и вывести ее на экран.
+
+try:
+    number_abc = int(input('Введите трехзначное целое число:'))
+    number_ab = number_abc // 10
+    number_c = number_abc % 10
+    number_a = number_ab // 10
+    number_b = number_ab % 10
+    sum_abc = number_a + number_b + number_c
+except ValueError as e:
+    print(e)
+else:
+    print('Сумма чисел равна:', sum_abc)
--- HomeTasks/arithmetics/calc_square.py
+++ HomeTasks/arithmetics/calc_square.py
@@ -1,16 +1,16 @@
 # калькулятор квадрата
 
-
 from decimal import Decimal
 
+try:
+    side_of_square = float(input('Введите значение длины стороны квадрата, мм:'))
+    s_square = side_of_square ** 2
+    p_square = side_of_square * 4
+    d_square = side_of_square * 2 ** 0.5
 
-side_of_square = input('Введите значение длины стороны квадрата, мм:')
-side_of_square = float(side_of_square)
-s_square = side_of_square ** 2
-p_square = side_of_square * 4
-d_square = side_of_square * 2 ** 0.5
-
-print('Площадь квадрата равна:', Decimal(s_square), 'мм2')
-print('Периметр квадрата равен:', Decimal(p_square), 'мм')
-d_square = Decimal(d_square)
-print('Длина диагонали квадрата равна:', round(d_square, 2), 'мм')
+    print('Площадь квадрата равна:', Decimal(s_square), 'мм2')
+    print('Периметр квадрата равен:', Decimal(p_square), 'мм')
+    d_square = Decimal(d_square)
+    print('Длина диагонали квадрата равна:', round(d_square, 2), 'мм')
+except ValueError as e:
+    print(e)
